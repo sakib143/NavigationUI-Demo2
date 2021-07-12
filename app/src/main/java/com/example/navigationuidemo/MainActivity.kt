a05d9e6664ad30e55c0479bc9c90c38974de4a34
DESKTOP-9RET899\syeds <syed.sakib143@gmail.com>
2021-07-12 11:07:11 +0530

    Toolbar added.......Sakib Syed

--- app/src/main/java/com/example/navigationuidemo/MainActivity.kt
+++ app/src/main/java/com/example/navigationuidemo/MainActivity.kt
@@ -2,13 +2,28 @@ package com.example.navigationuidemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolBar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
