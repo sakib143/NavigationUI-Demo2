44ef976a124dab34c803f59f667b647a97c1dd38
DESKTOP-9RET899\syeds <syed.sakib143@gmail.com>
2021-07-14 17:04:20 +0530

    Burger menu added.......Sakib Syed

--- app/src/main/java/com/example/navigationuidemo/home/MainActivity.kt
+++ app/src/main/java/com/example/navigationuidemo/home/MainActivity.kt
@@ -6,10 +6,7 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.example.navigationuidemo.NavigationDirections
 import com.example.navigationuidemo.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -29,16 +26,17 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
 
         appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.homeFragment2,R.id.settingsFragment,  R.id.notificationFragment))
+            setOf(R.id.homeFragment2,R.id.settingsFragment,  R.id.notificationFragment), drawer_layout)
 
         setupActionBarWithNavController(navController,appBarConfiguration)
 
         //Setup Bottom Navigation
         bottomNav.setupWithNavController(navController)
+        navigationDrawer.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(drawer_layout) || super.onSupportNavigateUp()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
